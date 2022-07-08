368e7d8c4e2274d8ce8003bfd218e5f95a4b7b3e
Jan Janeczko <janeczko.jan@hotmail.com>
2022-07-08 22:15:25 +0200

    prepare layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
 	declarations: [
+		HeaderComponent,
 		AppComponent
 	],
 	imports: [
